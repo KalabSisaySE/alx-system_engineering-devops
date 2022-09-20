b4dc3a67bf771d4afb6bc8c7783db3efdd12716d
KalabSisaySE <kyimusic11@gmail.com>
2022-09-20 14:43:02 -0700

    fix task-5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,5 @@
 # Install Nginx web server (w/ Puppet)
-
-exec { 'Install':
-  command  =>'apt-get -y update && apt-get -y install nginx && echo "Holberton School" > /var/www/html/index.nginx-debian.html && command="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" && sed -i "s/server_name _;/$command/" /etc/nginx/sites-available/default && service nginx start',
-  provider => 'shell',
+exec { 'installNginx':
+  command  =>  'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ;  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  provider =>  shell,
 }
