207bb67b9021969888ba8e819926070dcc6056f5
KalabSisaySE <kyimusic11@gmail.com>
2022-11-01 00:44:52 +0300

    change dict value accessing method

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""the `0-gather_data_from_an_API` module retrieves employee data from API"""
+"""the `0-gather_data_from_an_API` module
+retrieves employee data from API
+"""
 from urllib import request
 from sys import argv
 import json
@@ -12,9 +14,9 @@ with request.urlopen(emp_url) as response:
 
 with request.urlopen(todo_url) as res:
     todos = json.loads(res.read())
-    total = [todo for todo in todos if todo["userId"] == int(argv[1])]
-    completed = [todo for todo in todos if todo["userId"]
-                 == int(argv[1]) and todo["completed"]]
+    total = [todo for todo in todos if todo.get("userId") == int(argv[1])]
+    completed = [todo for todo in todos if todo.get("userId")
+                 == int(argv[1]) and todo.get("completed")]
 
 print(
     "Employee {} is done with tasks ({}/{})".format(
