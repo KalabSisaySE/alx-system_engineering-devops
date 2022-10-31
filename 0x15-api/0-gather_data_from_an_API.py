a52af57fd92d2728571e7be63152550c4839aa44
KalabSisaySE <kyimusic11@gmail.com>
2022-11-01 01:19:15 +0300

    fix task-0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,21 +5,22 @@ from sys import argv
 from urllib import request
 from sys import argv
 
-emp_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-todo_url = "https://jsonplaceholder.typicode.com/todos/"
+if __name__ == "__main__":
+    emp_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todo_url = "https://jsonplaceholder.typicode.com/todos/"
 
-with request.urlopen(emp_url) as response:
-    emp_data = json.loads(response.read())
+    with request.urlopen(emp_url) as response:
+        emp_data = json.loads(response.read())
 
-with request.urlopen(todo_url) as res:
-    todos = json.loads(res.read())
-    total = [todo for todo in todos if todo.get("userId") == int(argv[1])]
-    completed = [todo for todo in todos if todo.get("userId")
-                 == int(argv[1]) and todo.get("completed")]
+    with request.urlopen(todo_url) as res:
+        todos = json.loads(res.read())
+        total = [todo for todo in todos if todo.get("userId") == int(argv[1])]
+        completed = [todo for todo in todos if todo.get("userId")
+                     == int(argv[1]) and todo.get("completed")]
 
-print(
-    "Employee {} is done with tasks ({}/{})".format(
-        emp_data["name"], len(completed), len(total))
-)
-for task in completed:
-    print("\t {}".format(task["title"]))
+    print(
+        "Employee {} is done with tasks ({}/{})".format(
+            emp_data["name"], len(completed), len(total))
+    )
+    for task in completed:
+        print("\t {}".format(task["title"]))
