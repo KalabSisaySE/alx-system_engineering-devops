0fd5930657d0372b07657062489649a9fd7053c0
KalabSisaySE <kyimusic11@gmail.com>
2022-11-01 00:34:16 +0300

    complete task-0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""the `0-gather_data_from_an_API` module
+retrieves employee data from API
+"""
+from urllib import request
+from sys import argv
+import json
+
+emp_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+todo_url = "https://jsonplaceholder.typicode.com/todos/"
+
+with request.urlopen(emp_url) as response:
+    emp_data = json.loads(response.read())
+
+with request.urlopen(todo_url) as res:
+    todos = json.loads(res.read())
+    total = [todo for todo in todos if todo["userId"] == int(argv[1])]
+    completed = [todo for todo in todos if todo["userId"]
+                 == int(argv[1]) and todo["completed"]]
+
+print(
+    "Employee {} is done with tasks ({}/{})".format(
+        emp_data["name"], len(completed), len(total))
+)
+for task in completed:
+    print("\t {}".format(task["title"]))
